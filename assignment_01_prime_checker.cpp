b8422887c7b4d23869788f03edd4d0f0afddf396
agyeiwaaowusu103-a11y <agyeiwaaowusu103@gmail.com>
2026-08-02 16:57:44 +0000

    Complete assignment 1

--- assignment_01_prime_checker.cpp
+++ assignment_01_prime_checker.cpp
@@ -33,7 +33,40 @@
 // =============================================================================
 // YOUR CODE BELOW — remove the // symbols from the scaffold and fill it in
 // =============================================================================
-
 #include <iostream>
 using namespace std;
 
+
+
+
+
+
+
+bool isPrime(int n)
+{
+    if (n < 2)
+        return false;
+
+    for (int i = 2; i < n; i++)
+    {
+        if (n % i == 0)
+            return false;
+    }
+
+    return true;
+}
+
+int main()
+{
+    int number;
+
+    cout << "Enter a number: ";
+    cin >> number;
+
+    if (isPrime(number))
+        cout << "Prime" << endl;
+    else
+        cout << "Not Prime" << endl;
+
+    return 0;
+}
